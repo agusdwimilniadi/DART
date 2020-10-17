30977fde724cefe0ebe8f18209dcd34d07263825
agusdwimilniadi <agusdwimill@gmail.com>
2020-10-17 23:19:59 +0700

    list

--- bin/my_first_dart_console.dart
+++ bin/my_first_dart_console.dart
@@ -1,5 +1,19 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  
+    List <int> myList = [1,2,3,4,5,6];
+
+
+    for (int i = 0 ; i< myList.length; i +=1){
+      print(myList[i]);
+    }
+
+    for (int bilangan in myList){
+      print(bilangan);
+    }
+    myList.forEach((element) {
+      print(element);
+    });
+
+    myList.insert(2, 10);
 }
